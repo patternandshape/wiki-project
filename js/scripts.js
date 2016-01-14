61463c6705e6ac89e1083cdaf3784417ac080542
Molly Curtin and torrence stratton <patternandshape@gmail.com>
2016-01-14 13:33:13 -0800

    add hyphen-stripping and add info panels

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,10 @@ $(document).ready(function() {
   var pagetitles = ["Markdown", "Block-elements", "Inline-elements", "CSS", "Bootstrap", "Divs-and-Spans", "Floats", "Box-model", "Cascading-Styles", "Grid-System", "Responsive-Design", "Javascript-Arithmetic", "JS-Variables", "JS-Methods", "JS-Strings", "JS-Functions", "jQuery", "DOM-Manipulation-and-Traversal", "Forms", "Attributes", "Branching", "Arrays", "Looping"];
 
   pagetitles.forEach(function(pagetitle) {
-    var linktitle = $(pagetitle);
-    $("ul").append("<li>" + "<a href=" + '"' + pagetitle + ".html" + '"' + ">" + pagetitle + "</a>" + "</li>")
+    var linktitle = pagetitle;
+    noDashLinkTitle = (linktitle).replace("-", " ");
+
+    $(".sidebar-nav").append("<li>" + "<a href=" + '"' + pagetitle + ".html" + '"' + ">" + noDashLinkTitle + "</a>" + "</li>")
 
   });
 });
