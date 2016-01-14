3dc4da2213271036ae723a80aad85031e110167b
Molly Curtin and torrence stratton <tlstratton@gmail.com>
2016-01-14 15:11:52 -0800

    add working form page

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,16 @@ $(document).ready(function() {
     $(".sidebar-nav").append("<li>" + "<a href=" + '"' + pagetitle + ".html" + '"' + ">" + noDashLinkTitle + "</a>" + "</li>")
 
   });
+
+    $("#blobs form").submit(function(event) {
+       var blobs = ["firstName", "lastName"];
+       blobs.forEach(function(blob) {
+       var userInput = $("input#" + blob).val();
+       $("." + blob).text(userInput);
+
+      $("#userInfo").show();
+
+      event.preventDefault();
+        });
+    });
 });
